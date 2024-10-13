9323c5f90eb5b0564e4fc2c05e69a203e45814df
master <nishanthgfg@gmail.com>
2024-10-12 18:27:42 -0700

    basic express app has been setup

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+import { DB_URL,PORT} from "./config/serverConfig.js"
+
+import app from "./app.js"
+
+(async () => {
+
+   try{
+
+     await mongoose.connect(DB_URL)
+     console.log("DB connected")
+
+     app.on("error",(err) => {
+ 
+       console.error("ERROR ",err)
+
+       throw err
+
+     })   //The db is connected and the express app doesn't have any trouble talking to it.
+
+
+     const onListening = () => {
+
+       console.log(`App is listening on PORT ${PORT}`)
+
+     }
+
+     app.listen(PORT,onListening)
+
+
+   }
+   catch(err)
+   {
+     console.error("ERROR: ",err)
+     throw err
+   }
+
+})()
